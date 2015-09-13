4f7ab8e73222ba22cdbcb586948348481c3ea75d
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2015-09-13 20:14:45 +0200

    bla bla

--- TestForBuild/Program.cs
+++ TestForBuild/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace TestForBuild
 {
-    public class Program
+    class Program
     {
-        public void Main(string[] args)
+        static void Main(string[] args)
         {
         }
     }
